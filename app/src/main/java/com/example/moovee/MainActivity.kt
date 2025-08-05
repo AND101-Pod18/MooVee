5ac6ac5c34b57a3d6b9ccab73e7bd4bc899b6036
sai-sweekruthi <posh7sai@gmail.com>
2025-08-04 19:52:51 -0700

    created basic UI - background, Spinners, TextView, Button, etc.

--- app/src/main/java/com/example/moovee/MainActivity.kt
+++ app/src/main/java/com/example/moovee/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.example.moovee
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val spinner1: Spinner = findViewById(R.id.genres_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.genres_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner1.adapter = adapter
+        }
+
+        val spinner2: Spinner = findViewById(R.id.years_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.years_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner2.adapter = adapter
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
